2b2b05ba70b32fa6d90ff02f3c23792eccb976ae
nakatakut2 <nakatakut2@gamil.com>
2023-07-11 12:00:41 +0900

    静的ハッシュを定数化

--- 07.ls_object/file_detail.rb
+++ 07.ls_object/file_detail.rb
@@ -5,6 +5,9 @@ require 'etc'
 class FileDetail
   attr_reader :file_name, :file_stat
 
+  TYPES = { 'file' => '-', 'directory' => 'd', 'link' => 'l' }.freeze
+  PERMISSIONS = { '0' => '---', '1' => '--X', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }.freeze
+
   def initialize(file_name)
     @file_name = file_name
     @file_stat = File.stat(file_name)
@@ -12,14 +15,12 @@ class FileDetail
 
   def ftype
     ftype_key = File.ftype(file_name)
-    types = { 'file' => '-', 'directory' => 'd', 'link' => 'l' }
-    types[ftype_key]
+    TYPES[ftype_key]
   end
 
   def permissions
     permission_numbers = file_stat.mode.to_s(8).slice(-3, 3).chars
-    permission_table = { '0' => '---', '1' => '--X', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-', '7' => 'rwx' }
-    permission_numbers.map { |num| permission_table[num] }.join
+    permission_numbers.map { |num| PERMISSIONS[num] }.join
   end
 
   def nlink
